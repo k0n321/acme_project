3f98cbc4d232194247c19b1e44c2813c7f1ea17e
Aleksei Demenok <lebumaga@gmail.com>
2025-06-18 15:49:50 +0200

    Implement birthday functionality: add models, forms, views, and templates; integrate django_bootstrap5 for styling

--- acme_project/birthday/views.py
+++ acme_project/birthday/views.py
@@ -1,14 +1,29 @@
-# birthday/views.py
+# birthday/views.py 
 from django.shortcuts import render
 
-# Импортируем класс BirthdayForm, чтобы создать экземпляр формы.
 from .forms import BirthdayForm
+# Импортируем из utils.py функцию для подсчёта дней.
+from .utils import calculate_birthday_countdown
 
+from .models import Birthday
 
 def birthday(request):
-    # Создаём экземпляр класса формы.
-    form = BirthdayForm()
-    # Добавляем его в словарь контекста под ключом form:
+    form = BirthdayForm(request.POST or None)
     context = {'form': form}
-    # Указываем нужный шаблон и передаём в него словарь контекста.
+    if form.is_valid():
+        form.save()
+        birthday_countdown = calculate_birthday_countdown(
+            # ...и передаём в неё дату из словаря cleaned_data.
+            form.cleaned_data['birthday']
+        )
+        # Обновляем словарь контекста: добавляем в него новый элемент.
+        context.update({'birthday_countdown': birthday_countdown})
     return render(request, 'birthday/birthday.html', context)
+
+
+def birthday_list(request):
+    # Получаем все объекты модели Birthday из БД.
+    birthdays = Birthday.objects.all()
+    # Передаём их в контекст шаблона.
+    context = {'birthdays': birthdays}
+    return render(request, 'birthday/birthday_list.html', context) 
